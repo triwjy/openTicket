49fd9979cb5015107f9f8316a0d45255cdee91d1
triwjy <ztriwijaya@gmail.com>
2022-01-21 15:00:17 +0800

    create create-ticket route

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,6 +3,7 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError } from '@twtix/common';
+import { createTicketRouter } from './routes/new';
 
 const app = express();
 // trust traffic that comes from proxy (ingress-nginx)
@@ -12,6 +13,8 @@ app.use(cookieSession({
   signed: false,
   secure: process.env.NODE_ENV != 'test'
 }));
+
+app.use(createTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
