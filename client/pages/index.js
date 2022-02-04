5c9e2410a9cc3707348d998a5c14684442a03724
triwjy <ztriwijaya@gmail.com>
2022-02-04 16:39:24 +0800

    show ticket list on index page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,19 +1,37 @@
 import React from 'react'
 
-const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
+const LandingPage = ({ currentUser, tickets }) => {
+  const ticketList = tickets.map(ticket => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+      </tr>
+    )
+  })
   return (
     <div>
-    {currentUser ? <h3>You are signed in</h3> 
-                 : <h3>Your are not signed in</h3> }
+      <h1>Tickets</h1>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {ticketList}
+        </tbody>
+      </table>
     </div>
   )
 }
 
 // executed first in ssr before rendering html (specific to next)
 // can do fetch data here
-LandingPage.getInitialProps = async (context) => {
-  return {}
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+  const { data } = await client.get('/api/tickets');
+  return { tickets: data };
 };
 
 export default LandingPage;
