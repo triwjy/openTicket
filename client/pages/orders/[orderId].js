197cfe85641fae28021315908b747380af27fe19
triwjy <ztriwijaya@gmail.com>
2022-02-04 18:54:30 +0800

    add stripe payment checkout

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,7 +1,20 @@
 import React, { useEffect, useState } from 'react';
+import useRequest from '../../hooks/use-request';
+import StripeCheckout from 'react-stripe-checkout';
 
-const OrderShow = ({ order }) => {
+const OrderShow = ({ order, currentUser }) => {
   const [timeLeft, setTimeLeft] = useState(0);
+
+  const { doRequest, errors } = useRequest({
+    url: '/api/payments',
+    method: 'post',
+    body: {
+      orderId: order.id
+    },
+    onSuccess: (payment) => {
+      console.log(payment);
+    }
+  })
 
   useEffect(() => {
     const findTimeLeft = () => {
@@ -21,7 +34,19 @@ const OrderShow = ({ order }) => {
     return <div>Order Expired</div>;
   }
 
-  return <div>Time left to pay: {timeLeft} seconds</div>;
+  return (
+    <div>
+      Time left to pay: {timeLeft} seconds
+      <StripeCheckout 
+        token={({ id }) => doRequest({ token: id })}
+        stripeKey={process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY}
+        amount={order.ticket.price}
+        email={currentUser.email}
+        currency='JPY'
+      />
+      { errors }
+    </div>
+  );
 };
 
 OrderShow.getInitialProps = async (context, client) => {
