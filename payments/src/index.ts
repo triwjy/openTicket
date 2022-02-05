dec08f475436cc3860b5ea141c388068b69e33e9
triwjy <ztriwijaya@gmail.com>
2022-02-05 16:30:58 +0800

    add start up message

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -30,6 +30,8 @@ const start = async () => {
 
 
   try {
+    console.log('Starting up Payments Service...');
+    
     await natsWrapper.connect(
       process.env.NATS_CLUSTER_ID,
       process.env.NATS_CLIENT_ID,
